746f90e4c20553012a3741f5a27ca7ce247e5f54
vhopf <vinzenz.hopf@hopf-hof.de>
2021-07-08 22:57:16 +0200

    add ModuleManager

---
+++ cube-controller/lib/CubeCore/Base/ModuleManager.cpp
@@ -0,0 +1,58 @@
+/*
+ * ModuleManager.cpp
+ *
+ * Created: 08.07.2017 22:38:52
+ * Author: Vinzenz Hopf (happyfreak.de)
+ */ 
+
+#include "ModuleManager.h"
+
+ModuleManager::ModuleManager(){
+    nModuleCount = 0;
+    eState = EModuleManagerState::Initialize;
+}
+
+ModuleManager::~ModuleManager(){
+}
+
+/**
+ * Registers a module for cyclic execution
+ */
+void ModuleManager::registerModule(CyclicModule* module){
+    arrModules[nModuleCount] = module;
+    nModuleCount++;
+}
+
+/**
+ * Method that should be called cyclic and contain the main procedure logic of this module.
+ */
+void ModuleManager::cyclic(){
+    switch (eState)
+    {
+    case EModuleManagerState::None:
+        eState = EModuleManagerState::Initialize;
+        break;
+    
+    case EModuleManagerState::Initialize:
+        bool bInitialized = true;
+        for(uint8_t i = 0; i < nModuleCount; i++){
+            if(!arrModules[i]->initialize()){
+                bInitialized = false;
+            }
+        }
+        if(bInitialized){
+            eState = EModuleManagerState::Running;
+        }
+        break;
+
+    case EModuleManagerState::Running:
+        for(uint8_t i = 0; i < nModuleCount; i++){
+            arrModules[i]->cyclic();
+        }
+        break;
+
+    default:
+        eState = EModuleManagerState::None;
+        break;
+    }
+}
---
+++ cube-controller/lib/CubeCore/Base/ModuleManager.h
@@ -0,0 +1,49 @@
+/*
+ * ModuleManager.h
+ *
+ * Created: 08.07.2017 22:39:5
+ * Author: Vinzenz Hopf (happyfreak.de)
+ */ 
+
+
+#ifndef __MODULEMANAGER_H__
+#define __MODULEMANAGER_H__
+
+#include <avr/io.h>
+#include "CyclicModule.h"
+
+#define MODULE_COUNT 10
+
+enum class EModuleManagerState{
+    None,
+    Initialize,
+    Running
+};
+
+class ModuleManager {
+    //Variables
+    private:	
+        CyclicModule* arrModules[MODULE_COUNT];
+        uint8_t nModuleCount;
+
+        EModuleManagerState eState;
+
+    //Methods
+    public:
+        ModuleManager();
+	    ~ModuleManager();
+
+        /**
+         * Registers a module for cyclic execution
+         */
+        void registerModule(CyclicModule* module);
+
+        /**
+         * Method that should be called cyclic and contain the main procedure logic of this module.
+         */
+        void cyclic();    
+
+    protected:
+};
+
+#endif //__MODULEMANAGER_H__
